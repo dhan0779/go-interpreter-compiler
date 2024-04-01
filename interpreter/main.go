e8deb2a6a15884af92f5a06800e7485d4a8daf72
David Han <dmh338@cornell.edu>
2024-04-01 18:50:22 -0400

    Add repl for CLI tokenizing

---
+++ interpreter/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the CLI!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
