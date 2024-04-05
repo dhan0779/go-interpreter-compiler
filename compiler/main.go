7cd279744d08e8a5f7025a70a1acd9679ca30c7a
David Han <dmh338@cornell.edu>
2024-04-05 16:55:15 -0400

    Add main file

---
+++ compiler/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"compilerxs/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the CLI!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
