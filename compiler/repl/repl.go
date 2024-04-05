c4fb2fc7be38ab6ecf154105207e6362e2ad81a0
David Han <dmh338@cornell.edu>
2024-04-05 16:54:39 -0400

    Add REPL for terminal access

---
+++ compiler/repl/repl.go
@@ -0,0 +1,60 @@
+package repl
+
+import (
+	"bufio"
+	"compiler/compiler"
+	"compiler/lexer"
+	"compiler/parser"
+	"compiler/vm"
+	"fmt"
+	"io"
+)
+
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Compilation failed:\n %s\n", err)
+			continue
+		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string){
+	for _, msg := range  errors {
+		io.WriteString(out, "\t" + msg + "\n")
+	}
+}
