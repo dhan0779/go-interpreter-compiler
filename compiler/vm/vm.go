67748e796155a43f6ea989b1755a2634219933c2
David Han <dmh338@cornell.edu>
2024-04-11 00:46:46 -0400

    Call functions with arguments in vm

--- compiler/vm/vm.go
+++ compiler/vm/vm.go
@@ -176,8 +176,15 @@ func (vm *VM) Run() error {
 				return err
 			}
 		case code.OpCall:
+			numArgs := code.ReadUint8(ins[ip+1:])
 			vm.currentFrame().ip += 1
-			fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
+
+			err := vm.callFunction(int(numArgs))
+			if err != nil {
+				return err
+			}
+
+			fn, ok := vm.stack[vm.sp-1-int(numArgs)].(*object.CompiledFunction)
 			if !ok {
 				return fmt.Errorf("calling non-function")
 			}
@@ -441,4 +448,17 @@ func (vm *VM) pushFrame(f *Frame) {
 func (vm *VM) popFrame() *Frame {
 	vm.framesIndex--
 	return vm.frames[vm.framesIndex]
+}
+
+func (vm *VM) callFunction(numArgs int) error {
+	fn, ok := vm.stack[vm.sp-1-numArgs].(*object.CompiledFunction)
+	if !ok {
+		return fmt.Errorf("calling non-function")
+	}
+
+	frame := NewFrame(fn, vm.sp - numArgs)
+	vm.pushFrame(frame)
+
+	vm.sp = frame.basePointer + fn.NumLocals
+	return nil
 }
