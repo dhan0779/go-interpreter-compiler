652b9302b1277b3a59e7a6ca1c6f9f1cb3b6996a
David Han <dmh338@cornell.edu>
2024-04-14 20:56:50 -0400

    Add function for calling builtins

--- compiler/vm/vm.go
+++ compiler/vm/vm.go
@@ -481,3 +481,17 @@ func (vm *VM) callFunction(fn *object.CompiledFunction, numArgs int) error {
 	vm.sp = frame.basePointer + fn.NumLocals
 	return nil
 }
+
+func (vm *VM) callBuiltin(builtin *object.Builtin, numArgs int) error {
+	args := vm.stack[vm.sp-numArgs : vm.sp]
+	result := builtin.Fn(args...)
+	vm.sp = vm.sp - numArgs - 1
+
+	if result != nil {
+		vm.push(result)
+	} else {
+		vm.push(Null)
+	}
+
+	return nil
+}
